d15177f1c81d9e6b809df82041c227ae3bea31e2
David Kanekanian <dkanekanian1@gmail.com>
2023-03-26 14:29:48 +0100

    adjustmet

--- src/particleSparkle.ts
+++ src/particleSparkle.ts
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { ParticleSystem } from "./particleSystem";
 
 const lerp = (a, b, x) => a + (b - a) * x;
 const vlen = (a, b) => Math.sqrt(a * a + b * b);
@@ -8,52 +9,8 @@ let { innerWidth: w, innerHeight: h } = window;
 let cam = new THREE.PerspectiveCamera(40, w / h, 1, 10000);
 cam.position.z = 300;
 let scene = new THREE.Scene();
-let t = 10000,
-  n = 0;
-let sm = new THREE.ShaderMaterial({
-  vertexShader: `
-  attribute float size;
-  varying vec3 vColor;
-  varying float fSize;
-  void main() {
-   vColor = color;
-   vec4 mvp = modelViewMatrix * vec4(position, 1.0);
-   gl_PointSize = size * (300.0 / -mvp.z);
-   fSize = gl_PointSize;
-   gl_Position = projectionMatrix * mvp;
-  }`,
-  fragmentShader: `
-  varying vec3 vColor;
-  varying float fSize;
-  void main() {
-   float dist = 1. - distance(gl_PointCoord, vec2(0.5, 0.5));
-   float a = pow(dist + 0.3, fSize/0.5);
-   gl_FragColor = vec4(vColor, a);
-  }`,
-  blending: THREE.AdditiveBlending,
-  depthTest: false,
-  transparent: true,
-  vertexColors: true,
-});
-let geo = new THREE.BufferGeometry();
-geo.setAttribute(
-  "position",
-  new THREE.Float32BufferAttribute(Array(t * 3).fill(0), 3)
-);
-geo.setAttribute(
-  "color",
-  new THREE.Float32BufferAttribute(Array(t * 3).fill(0), 3)
-);
-geo.setAttribute("size", new THREE.Float32BufferAttribute(Array(t).fill(0), 1));
-// @ts-ignore
-geo.getAttribute("position").setUsage(THREE.DynamicDrawUsage);
-// @ts-ignore
-geo.getAttribute("color").setUsage(THREE.DynamicDrawUsage);
-// @ts-ignore
-geo.getAttribute("size").setUsage(THREE.DynamicDrawUsage);
-geo.setDrawRange(0, 0);
 
-let ps = new THREE.Points(geo, sm);
+let ps = new ParticleSystem()
 scene.add(ps);
 let rend = new THREE.WebGLRenderer();
 rend.setPixelRatio(window.devicePixelRatio);
